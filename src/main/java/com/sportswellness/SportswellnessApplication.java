2e470ac848d9f8b548841177c5e769a59f7ecf01
202178050 <alsehd2414@naver.com>
2025-03-12 19:56:54 +0900

    초기 프로젝트 커밋

---
+++ src/main/java/com/sportswellness/SportswellnessApplication.java
@@ -0,0 +1,13 @@
+package com.sportswellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportswellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportswellnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportswellness/SportswellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportswellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportswellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
